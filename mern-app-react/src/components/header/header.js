5bc89cae5b79f305f026c0b598de7a8016b2d321
SrijaniSom <srijanisom2002@gmail.com>
2024-10-22 18:19:44 +0530

    setup of the navbar is done

--- mern-app-react/src/components/header/header.js
+++ mern-app-react/src/components/header/header.js
@@ -1,9 +1,20 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+
 const Header = () => {
     return (
         <>
-            <h1>
-                Header comp
-            </h1>
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand to="/" as={Link}>My App</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link to="/" as={Link}>Dashboard</Nav.Link>
+                        <Nav.Link to="/user" as={Link}>Post User</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
         </>
 
     )
